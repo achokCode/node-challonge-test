cd5730445cb58f39c3871ccd86479f6e4aac91cf
achokb02 <achokb02@g.uafs.edu>
2022-02-27 22:56:53 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, component } from 'react';
+import axios from 'axios';
 import './App.css';
+import { TournamentAdapter } from 'challonge-ts';
 
 function App() {
+  const test = async () => {
+    const data = await TournamentAdapter.index('ooJ5o12aTrs5SdDdqo1YBYo0ZevUcVC1fWL0pZy2');
+    console.log(data);
+  }
+
+  test(); // Direct response from the API, array of tournament JSON objects
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello to the world!!
     </div>
   );
 }
 
 export default App;
+
